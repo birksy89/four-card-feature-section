87bcf59c199e719b60c9e3f2ff4cffbdc76749e7
Andrew <andrew@bbm.co.uk>
2020-02-27 18:28:55 +0000

    Adding Stripe

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,29 @@ import calculator from "../images/icon-calculator.svg";
 
 const StyledCard = styled.div`
   font-family: "Poppins";
+  background-color: #fff;
+  border-radius: 15px;
+
+  overflow: hidden;
+`;
+const StyledStripe = styled.div`
+  background-color: red;
+  height: 15px;
+  width: 100%;
+`;
+const TextWrapper = styled.div`
+  margin: 15px;
 `;
 
 export default function Card({ title, children }) {
   return (
     <StyledCard>
-      <h3>{title}</h3>
-      {children}
-      <img src={calculator} />
+      <StyledStripe />
+      <TextWrapper>
+        <h3>{title}</h3>
+        <div>{children}</div>
+        <img src={calculator} />
+      </TextWrapper>
     </StyledCard>
   );
 }
