9d19d6675113559ae7f328ac430dca66f87692a5
Andrew <andrew@bbm.co.uk>
2020-02-27 17:04:45 +0000

    Moving inital designs over

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Reliable, efficient delivery Powered by Technology Our Artificial
+          Intelligence powered tools use millions of project data points to
+          ensure that your project is successful Supervisor Monitors activity to
+          identify project roadblocks Team Builder Scans our talent network to
+          create the optimal team for your project Karma Regularly evaluates our
+          talent to ensure quality Calculator Uses data from past projects to
+          provide better delivery estimates
         </p>
         <a
           className="App-link"
